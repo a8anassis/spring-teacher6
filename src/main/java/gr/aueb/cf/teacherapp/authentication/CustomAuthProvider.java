ac882468ebc016a4aa170a45a37d9d0520093c41
Th. Androutsos <a8anassis@gmail.com>
2024-10-16 20:26:40 +0300

    auth

--- src/main/java/gr/aueb/cf/teacherapp/authentication/CustomAuthProvider.java
+++ src/main/java/gr/aueb/cf/teacherapp/authentication/CustomAuthProvider.java
@@ -33,6 +33,7 @@ public class CustomAuthProvider {
         return config.getAuthenticationManager();
     }
 
+    @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder(11);
         //return NoOpPasswordEncoder.getInstance();
---
+++ src/main/java/gr/aueb/cf/teacherapp/service/UserService.java
@@ -0,0 +1,22 @@
+package gr.aueb.cf.teacherapp.service;
+
+
+import gr.aueb.cf.teacherapp.model.User;
+import gr.aueb.cf.teacherapp.repository.UserRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    @Transactional
+    public Optional<User> findUserByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+}
